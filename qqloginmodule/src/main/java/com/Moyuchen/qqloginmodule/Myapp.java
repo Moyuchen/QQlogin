13370735474246bbe9a720326460aa54890b1adb
Moyuchen <31385093+Moyuchen@users.noreply.github.com>
2017-11-11 13:33:00 +0800

    Initial commit

---
+++ qqloginmodule/src/main/java/com/Moyuchen/qqloginmodule/Myapp.java
@@ -0,0 +1,18 @@
+package com.Moyuchen.qqloginmodule;
+
+import android.app.Application;
+
+/**
+ * User: 张亚博
+ * Date: 2017-09-10 19:58
+ * Description：
+ */
+public class Myapp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
